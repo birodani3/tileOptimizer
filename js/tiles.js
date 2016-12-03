b6e33e557872fbaf954c18097a5073783cc85fa3
birodani3 <birodani3@gmail.com>
2016-12-03 18:47:56 +0100

    Fix typo

--- js/tiles.js
+++ js/tiles.js
@@ -134,7 +134,7 @@ APP.tiles = (function() {
                 toCheck.push.call(toCheck, {x: 0, y: -1}, {x: 1, y: -1}, {x: 2, y: 0}, {x: 2, y: 1}, {x: 1, y: 2}, {x: 0, y: 2}, {x: -1, y: 1}, {x: -1, y: 0});
                 break;
             case 2:
-                toCheck.push.call(toCheck, {x: -1, y: -1}, {x: 0, y: -1}, {x: 1, y: 0}, {x: 1, y: 11}, {x: 0, y: 2}, {x: -1, y: 2}, {x: -2, y: 1}, {x: -2, y: 0});
+                toCheck.push.call(toCheck, {x: -1, y: -1}, {x: 0, y: -1}, {x: 1, y: 0}, {x: 1, y: 1}, {x: 0, y: 2}, {x: -1, y: 2}, {x: -2, y: 1}, {x: -2, y: 0});
                 break;
             case 3:
                 toCheck.push.call(toCheck, {x: 0, y: -2}, {x: 1, y: -2}, {x: 2, y: -1}, {x: 2, y: 0}, {x: 1, y: 1}, {x: 0, y: 1}, {x: -1, y: 0}, {x: -1, y: -1});
